b3bd44065e1fcc935d1f2ecd6ca985dfc9d5fd5b
Ojus <your.ojus.patil@gmail.com>
2013-11-10 21:52:40 -0800

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,6 +12,11 @@ describe "StaticPages" do
     	# Use the page variable from Capybara to expect matching content
     	expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do 
+    	visit '/static_pages/home'
+    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end 
 
 
@@ -25,6 +30,12 @@ describe "StaticPages" do
     	# Use the page variable from Capybara to expect matching content
     	expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do 
+    	visit '/static_pages/help'
+    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+
   end
 
     # Repeat for about page 
@@ -33,6 +44,11 @@ describe "StaticPages" do
   	it "should have the content 'About Us'" do
     	visit '/static_pages/about'
     	expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do 
+    	visit '/static_pages/about'
+    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
